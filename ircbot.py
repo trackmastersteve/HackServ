077181fb539d8d264b92ff5b4328bff08b93c46c
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-08-04 16:51:05 -0700

    Added port variable

--- ircbot.py
+++ ircbot.py
@@ -27,13 +27,14 @@ import socket
 
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server = "chat.freenode.net" # Server
+port = 6667 # Port
 channel = "#channel" # Channel
 botnick = "botnick" # Your bots IRC nick
 adminname = "master" # Your IRC nick
 exitcode = "bye " + botnick
 
 
-ircsock.connect((server, 6667)) # Here we connect to the server using the port 6667.
+ircsock.connect((server, port)) # Here we connect to the server.
 ircsock.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick +" "+ botnick + " " + botnick + "\n", "UTF-8")) # We are basically filling out a form with this line and saying to set all the fields to the bot nickname.
 ircsock.send(bytes("NICK "+ botnick +"\n", "UTF-8")) # Assign the nick to the bot.
 
