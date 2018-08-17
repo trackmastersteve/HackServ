0d4c884fef76b14f89cd2c01768db3785c1d33b4
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-08-17 16:06:03 -0700

    Added .join and .part commands

--- bot.py
+++ bot.py
@@ -24,8 +24,8 @@
 
 NOTICE = 'THIS BOT IS FOR EDUCATION PURPOSES ONLY! DO NOT USE IT FOR MALICIOUS INTENT!'
 author = 'Stephen Harris (trackmastersteve@gmail.com)'
-version = '0.1.7'
-last_modification = '2018.08.16'
+version = '0.1.9'
+last_modification = '2018.08.17'
 
 import ssl
 import nmap
@@ -47,7 +47,7 @@ exitcode = "bye " + botnick
 ##### Bot Settings #####
 ########################
 
-starttime = datetime.datetime.utcnow()
+starttime = datetime.datetime.utcnow() # Start time is used to calculate uptime.
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ircsock = ssl.wrap_socket(ircsock) # Comment this line out if you don't want to use SSL.
 ircsock.connect((server, port)) # Here we connect to the server.
@@ -63,6 +63,14 @@ def joinchan(chan): # Join channel(s).
         ircmsg = ircmsg.strip('\n\r')
         print(ircmsg)
 
+def partchan(chan): # Join channel(s).
+    ircsock.send(bytes("PART "+ chan +"\n", "UTF-8"))
+    ircmsg = ""
+    while ircmsg.find("End of /NAMES list.") == -1:
+        ircmsg = ircsock.recv(2048).decode("UTF-8")
+        ircmsg = ircmsg.strip('\n\r')
+        print(ircmsg)
+        
 def ping(): # Respond to server Pings.
     ircsock.send(bytes("PONG :pingis\n", "UTF-8"))
 
@@ -96,11 +104,11 @@ def main():
             message = ircmsg.split('PRIVMSG',1)[1].split(':',1)[1]
 
             if len(name) < 17:
-                # Respond to anyone telling bot 'Hi'.
+                # Respond to anyone saying 'Hi [botnick]'.
                 if message.find('Hi ' + botnick) != -1:
                     sendmsg("Hello " + name + "!")
 
-                # Respond to .tell [target] [message] command from anyone.
+                # Respond to '.tell [target] [message]' command from anyone.
                 if message[:5].find('.tell') != -1:
                     target = message.split(' ', 1)[1]
                     if target.find(' ') != -1:
@@ -111,22 +119,44 @@ def main():
                         message = "Could not parse. The message should be in format of '.tell [target] [message]' to work properly."
                     sendmsg(message, target)
 
-                # Respond to .ip command from admin.
+                # Respond to the '.join [channel]' command from admin.
+                if name.lower() == adminname.lower() and message[:5].find('.join') != -1:
+                    target = message.split(' ', 1)[1]
+                    if target.find(' ') != -1:
+                        message = "Ok, I will join the channel: " + target.split(' ')[0]
+                        target = target.split(' ')[0]
+                        joinchan(target)
+                    else:
+                        message = "Could not parse. The message should be in the form of '.join [channel]' to work properly."
+                    sendmsg(message, name)
+                
+                #Respond to the '.part [channel]' command from admin.
+                if name.lower() == adminname.lower() and message[:5].find('.part') != -1:
+                    target = message.split(' ', 1)[1]
+                    if target.find(' ') != -1:
+                        message = "Ok, I will part the channel: " + target.split(' ')[0]
+                        target = target.split(' ')[0]
+                        partchan(target)
+                    else:
+                        message = "Could not parse. The message should be in the form of '.part [channel]' to work properly."
+                    sendmsg(message, name)
+
+                # Respond to '.ip' command from admin.
                 if name.lower() == adminname.lower() and message.find('.ip') != -1:
                     sendmsg("My public ip address is:", name)
                     sendmsg(format(ip), name)
                 
-                # Respond to .uptime command from admin.
+                # Respond to '.uptime' command from admin.
                 if name.lower() == adminname.lower() and message.find('.uptime') != -1:
                     sendmsg("My current uptime:", name)
                     sendmsg(format(uptime()), name)
                     
-                # Respond to .uname command from admin.
+                # Respond to '.uname' command from admin.
                 if name.lower() == adminname.lower() and message.find('.uname') != -1:
                     sendmsg("System Info:", name)
                     sendmsg(format(uname()), name)
 
-                # Respond to .scan [target] command from admin.
+                # Respond to '.scan [target] [port(s)]' command from admin.
                 if name.lower() == adminname.lower() and message[:5].find('.scan') != -1:
                     target = message.split(' ', 1)[1]
                     if target.find(' ') != -1:
