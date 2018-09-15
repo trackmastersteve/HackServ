0a6bc391a653dd4c180645656b18352fcb43cfd3
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-09-15 16:57:49 -0700

    version bump

--- bot.py
+++ bot.py
@@ -26,8 +26,8 @@ legal_notice = 'THIS BOT IS FOR EDUCATION PURPOSES ONLY! DO NOT USE IT FOR MALIC
 author = 'Stephen Harris (trackmastersteve@gmail.com)'
 github = 'https://github.com/trackmastersteve/bot.git'
 software = 'arm0red bot'
-version = '0.6.6'
-last_modification = '2018.09.13'
+version = '0.6.7'
+last_modification = '2018.09.15'
 
 import ssl
 import sys
