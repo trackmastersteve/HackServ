1d2af36ce868f858745b23b7d84f382a6fe5ffde
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-10-06 15:38:14 -0700

    version bump

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ legal_notice = 'THIS BOT IS FOR EDUCATION PURPOSES ONLY! DO NOT USE IT FOR MALIC
 author = 'Stephen Harris (trackmastersteve@gmail.com)'
 github = 'https://github.com/trackmastersteve/bot.git'
 software = 'arm0red bot'
-version = '0.8.9'
+version = '0.9.0'
 last_modification = '2018.10.06'
 
 # Imports
