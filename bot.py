cda6a8347c3b84234aac0e2badbdb51e254af346
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-10-24 20:16:09 -0700

    mod date change

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ author = 'Stephen Harris (trackmastersteve@gmail.com)'
 github = 'https://github.com/trackmastersteve/bot.git'
 software = 'arm0red bot'
 version = '0.9.7'
-last_modification = '2018.10.21'
+last_modification = '2018.10.24'
 
 # Imports
 import os
