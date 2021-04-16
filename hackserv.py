2c470b36a152f3d43d5f570bb15d065633e331a2
Stephen Harris <trackmastersteve@users.noreply.github.com>
2021-04-16 11:56:24 -0700

    Updated last mod date

--- hackserv.py
+++ hackserv.py
@@ -28,7 +28,7 @@ author = 'Stephen Harris (trackmastersteve@gmail.com)'
 github = 'https://github.com/trackmastersteve/hackserv.git'
 software = 'HackServ'
 version = '1.1.2'
-last_modification = '2021.04.15'
+last_modification = '2021.04.16'
 
 # Imports
 import os
