a89c23709b1847fbdf83c96eacb92773e86f774d
Stephen Harris <trackmastersteve@users.noreply.github.com>
2021-04-25 21:04:16 -0700

    Updated version and last mod date

--- hackserv.py
+++ hackserv.py
@@ -27,8 +27,8 @@ legal_notice = 'THIS BOT IS FOR EDUCATION PURPOSES ONLY! DO NOT USE IT FOR MALIC
 author = 'Stephen Harris (trackmastersteve@gmail.com)'
 github = 'https://github.com/trackmastersteve/hackserv.git'
 software = 'HackServ'
-version = '1.1.5'
-last_modification = '2021.04.19'
+version = '1.1.6'
+last_modification = '2021.04.25'
 
 # Imports
 import os
