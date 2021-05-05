66f505eb90326ccd19973e8d3fa994cb209e6c39
Stephen Harris <trackmastersteve@users.noreply.github.com>
2021-05-05 08:46:36 -0700

    Added '.ls [dir]' to help menu

--- hackserv.py
+++ hackserv.py
@@ -605,6 +605,7 @@ def main(): # This is the main function for all of the bot controls.
                                   '.mode [target] [mode]' (set mode on nick or channel),
                                   '.nick [newnick]' (sets a new botnick),
                                   '.raw [command]' (sends a raw command to the IRC server),
+                                  '.ls [dir]' (lists files in a directory),
                                   '.dl [url] [file]' (downloads [url] and saves as [file]),
                                   '.run [execute type]' [executable file]' (execute a file),
                                   '.upgrade [link] [file]' (upgrades the hackserv.py file),
