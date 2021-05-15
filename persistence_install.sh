89b9f0d7df1dd3d8b2d242d182370da4cd4c3ce1
Stephen Harris <trackmastersteve@users.noreply.github.com>
2021-05-15 09:12:31 -0700

    Update persistence_install.sh

--- persistence_install.sh
+++ persistence_install.sh
@@ -6,11 +6,12 @@
 echo "This Script will install Hackserv as a Systemd Service."
 echo "Creating 'HackServ' user..."
 sudo useradd -s /bin/nologin hackserv
-echo "copying hackserv.py to /usr/local/bin/..."
+echo "Hiding hackserv.py in /usr/local/bin/..."
 sudo cp hackserv.py /usr/local/bin/.hackserv.py
-echo "copying hsConfig.py to /usr/local/bin/..."
+echo "Copying hsConfig.py to /usr/local/bin/..."
 sudo cp hsConfig.py /usr/local/bin/hsConfig.py
-echo "copying hackserv.service to /etc/systemd/system..."
+sudo vim /usr/local/bin/hsConfig.py
+echo "Copying hackserv.service to /etc/systemd/system..."
 sudo cp hackserv.service /etc/systemd/system/hackserv.service
 echo "Starting HackServ..."
 sudo systemctl enable --now hackserv.service
