1371f6beaa626a1df4e6eb4deb638d2131280bc2
Stephen Harris <trackmastersteve@users.noreply.github.com>
2021-05-15 09:32:02 -0700

    Added config_version

--- hsConfig.py
+++ hsConfig.py
@@ -23,6 +23,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 #
+config_version = 1.2
 import random
 from requests import get
 ip = get('https://api.ipify.org').text
